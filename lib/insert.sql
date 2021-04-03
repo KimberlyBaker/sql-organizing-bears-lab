185c1f277ef373f35a13b7f84d153f5c704fbcdc
Kimberly <bakerk2010@gmail.com>
2021-04-03 14:25:27 -0400

    Pushing all file to GitHub.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, tempermanet, alive) 
+VALUES ("Mr. Chocolate", 6 , "Male", "Brown", "Calm", 1);
+
+INSERT INTO bears (name, age, gender, color, tempermanet, alive) 
+VALUES ("Rowdy", 2 , "Male", "Black", "Playful", 0);
+
+INSERT INTO bears (name, age, gender, color, tempermanet, alive) 
+VALUES ("Tabitha", 3 , "Female", "Blonde", "Sassy", 1);
+
+INSERT INTO bears (name, age, gender, color, tempermanet, alive) 
+VALUES ("Sergeant Brown", 1 , "Male", "Cinnamon", "Angry", 1);
+
+INSERT INTO bears (name, age, gender, color, tempermanet, alive) 
+VALUES ("Melissa", 8 , "Female", "White", "Shy", 0);
+
+INSERT INTO bears (name, age, gender, color, tempermanet, alive) 
+VALUES ("Grinch", 4 , "Male", "Blue-gray", "Mean", 1);
+
+INSERT INTO bears (name, age, gender, color, tempermanet, alive) 
+VALUES ("Wendy", 5 , "Female", "Black", "Anxious", 0);
+
+INSERT INTO bears (name, age, gender, color, tempermanet, alive) 
+VALUES (NULL, 3 , "Male", "Brown", "Defensive", 0);
